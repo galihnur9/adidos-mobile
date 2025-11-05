c9b88f89f4fb49b10959d5732357865d9439a3f2
galihnur9 <galihnurr9@gmail.com>
2025-11-05 07:07:59 +0700

    Update Komponen ItemCard untuk layout app

---
+++ lib/menu.dart
@@ -0,0 +1,113 @@
+import 'package:flutter/material.dart';
+class MyHomePage extends StatelessWidget {
+  MyHomePage({super.key});
+  final List<ItemHomepage> items = [
+    ItemHomepage("All Products", Icons.inventory, Colors.blue),
+    ItemHomepage("My Products", Icons.shopping_bag, Colors.green),
+    ItemHomepage("Create Product", Icons.add_box, Colors.red),
+  ];
+  @override
+  Widget build(BuildContext context) {
+    // Scaffold menyediakan struktur dasar halaman dengan AppBar dan body.
+    return Scaffold(
+      // AppBar adalah bagian atas halaman yang menampilkan judul.
+      appBar: AppBar(
+        // Judul aplikasi "Adidos" dengan teks putih dan tebal.
+        title: const Text(
+          'Adidos',
+          style: TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        // Warna latar belakang AppBar diambil dari skema warna tema aplikasi.
+        backgroundColor: Theme.of(context).colorScheme.primary,
+      ),
+      // Body halaman dengan padding di sekelilingnya.
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        // Menyusun widget secara vertikal dalam sebuah kolom.
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              GridView.count(
+                primary: true,
+                padding: const EdgeInsets.all(20),
+                crossAxisSpacing: 10,
+                mainAxisSpacing: 10,
+                crossAxisCount: 3,
+                shrinkWrap: true,
+                children: items.map((ItemHomepage item) {
+                  return ItemCard(item);
+                }).toList(),
+              )
+            ]
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class ItemCard extends StatelessWidget {
+  // Menampilkan kartu dengan ikon dan nama.
+
+  final ItemHomepage item; 
+
+  const ItemCard(this.item, {super.key}); 
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      // Menentukan warna latar belakang dari tema aplikasi.
+      color: item.color,
+      // Membuat sudut kartu melengkung.
+      borderRadius: BorderRadius.circular(12),
+
+      child: InkWell(
+        // Aksi ketika kartu ditekan.
+        onTap: () {
+          // Menampilkan pesan SnackBar saat kartu ditekan.
+          ScaffoldMessenger.of(context)
+            ..hideCurrentSnackBar()
+            ..showSnackBar(
+              SnackBar(content: Text("Kamu telah menekan tombol ${item.name}!"))
+            );
+        },
+        // Container untuk menyimpan Icon dan Text
+        child: Container(
+          padding: const EdgeInsets.all(8),
+          child: Center(
+            child: Column(
+              // Menyusun ikon dan teks di tengah kartu.
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(
+                  item.icon,
+                  color: Colors.white,
+                  size: 30.0,
+                ),
+                const Padding(padding: EdgeInsets.all(3)),
+                Text(
+                  item.name,
+                  textAlign: TextAlign.center,
+                  style: const TextStyle(color: Colors.white),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+}
+
+class ItemHomepage {
+ final String name;
+ final IconData icon;
+ final Color color;
+
+ ItemHomepage(this.name, this.icon, this.color);
+}
